4e6e506d758dc52f5973cd07eb7d043df2e3e2c8
markmurray <m@rkmurray.co>
2021-03-24 21:23:31 +1100

    Attempt 2

--- src/exercise.py
+++ src/exercise.py
@@ -1,10 +1,8 @@
 def main():
     #write your code below this line
-    print("""
-    Once upon a time
-    there was
-    a program
-    """)
+    print("Once upon a time")
+    print("there was")
+    print("a program")
 
 if __name__ == '__main__':
     main()
